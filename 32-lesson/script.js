d945592fb368e10f3b7059d864cb20a0ee8b4a8b
Sasha Raimov <crazyysasha@gmail.com>
2024-02-01 12:16:52 +0500

    fix 32 lesson

---
+++ 32-lesson/script.js
@@ -0,0 +1,37 @@
+
+
+
+
+/**
+ * вывод на консоль
+ */
+console.log("hello \nJavohir");
+
+// Обозночение строки
+// ''
+// ""
+// ``
+
+
+console.log(`hello "Javohir"`);
+
+// числа
+1233 
+
+
+console.log(5 + 7 / 7);
+
+
+
+// alert('hello!');
+
+
+// переменные: var, let, const
+
+
+
+var vozrast = 14
+
+vozrast = 13
+
+console.log(2024 - vozrast);
